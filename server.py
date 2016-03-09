c8ecb0066c965b82c2404b8c42d30461556fef43
Artiom <aprotasenya@gmail.com>
2016-03-09 10:12:30 +0000

    new server.py

--- server.py
+++ server.py
@@ -1,44 +1,30 @@
+#!/usr/bin/python
+
+import asyncore
 import socket
-import os
 
+class EchoHandler(asyncore.dispatcher_with_send):
+    def handle_read(self):
+        data = self.recv(1024)
+        if data:
+            if str(data) == "close":
+                self.close()
+            else:
+                print data
+                self.send(data)
 
-
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('0.0.0.0', 2222))
-s.listen(10)
-
-pids = []
-try:
-  print('Starting listening on port 2222...')
-  while True:
-    conn, addr = s.accept()
-    print('New connection', addr)
-    newpid = os.fork()
-    if newpid != 0:
-      pids.append(newpid)
-    else:
-      print('Opening socket:', addr, os.getpid())
-      while True:
-        try:
-          data = conn.recv(1024)
-        except:
-          continue
-          
-        if not data or data.decode('utf-8') == 'close\r\n':
-          break
-        else:
-          conn.send(data)
-      #
-      print('Closing socket: ', addr, os.getpid())
-      conn.close()
-      print('Closed: ', addr)
-      os._exit(0)
-  #
-  for pid in pids:
-    print("Pid exited", pid)
-    os.waitpid(pid, 0)
-finally:
-  print('Closing server socket...')
-  s.close()
-  print('Closed.')
+class EchoServer(asyncore.dispatcher):
+    def __init__(self, host, port):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.set_reuse_addr()
+        self.bind((host, port))
+        self.liste(10)
+    def handle_accept(self):
+        pair = self.accept()
+        if pair is not None:
+            sock, addr = pair
+            print 'conn from %s' % repr(addr)
+            handler = EchoHandler(sock)
+server = EchoServer('0.0.0.0', 2222)
+asyncore.loop()
