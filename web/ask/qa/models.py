9e3c0b2369aed984e74a43457d11c75c91ffc24e
Artuom <aprotasenya@gmail.com>
2016-03-14 12:00:07 +0000

    added models

--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -2,4 +2,30 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True)
+    rating = models.IntegerField()
+    author = models.ForeignKey(User)
+    likes = User()
+    def __str__(self):
+        return self.title
+    def get_absolute_url(self):
+        return '/question/%d/' % self.pk
+    class Meta:
+        db_table = 'blogposts'
+        ordering = ['-added_at']
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True)
+    question = models.ForeignKey(Question, null=True,
+        on_delete = models.DO_NOTHING)
+    author = models.ForeignKey(User)
+    def __str__(self):
+        return self.title
