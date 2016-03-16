13c6c4f691650968f2e175af2386d57e43f4837a
mix-87 <aprotasenya@gmail.com>
2016-03-16 19:19:44 +0300

    templates

--- web/ask/qa/urls.py
+++ web/ask/qa/urls.py
@@ -3,13 +3,13 @@ from django.conf.urls import patterns, url
 from qa import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.test, name='test'),
+    url(r'^$', views.questions_all, name='questions_all'),
     url(r'^login', views.test, name='test'),
     url(r'^ask', views.test, name='test'),
     url(r'^signup', views.test, name='test'),
-    url(r'^popular', views.test, name='test'),
+    url(r'^popular', views.popular_questions, name='popular_questions'),
     url(r'^new', views.test, name='test'),
-    url(r'^question/(?P<question_id>\d+)/$', views.test, name='test'),
+    url(r'^question/(?P<question_id>\d+)/$', views.question_details, name='question_details'),
 )
 
 """
